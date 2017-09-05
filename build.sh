195c639f29b4727795fca9373b8ed1ed2c99ba2f
root <jasercion.gwu@gmail.com>
2017-09-05 22:38:02 +0000

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+echo "Building Fermi ScienceTools Test Suite"
+export CXXFLAGS="-I${PREFIX}/include -O2 -Wall --pedantic"
+export LDFLAGS="-L${PREFIX}/lib"
+export PYTHON="python2.7"
+export PYTHON_DIR="-I${PREFIX}/include/${PYTHON}"
+export PYTHON_INC="-I${PREFIX}/include/${PYTHON}"
+export CLHEP="CLHEP"
+export HOOPS="hoops_1.1"
+export PIL="ape_2.9"
+export CFITSIO="cfitsio"
+export READLINE="readline"
+export CPPUNIT="cppunit"
+export FFTW="fftw3"
+export XERCES="xerces-c"
+export BUILD_WITHOUT_ROOT="yes"
+./configure
+make
